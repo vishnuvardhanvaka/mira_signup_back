fa87644775a5abf654aaa5ca86ad83ec40696cfe
Vishnu Vardhan Gowd Vaka <vishnuvardhanvaka1@gmail.com>
2024-01-10 07:57:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,116 @@
-from asyncio import run
-from fastapi import Depends, FastAPI, HTTPException, status,Form,Response,Request
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from datetime import datetime,timedelta
-from jose import JWTError,jwt
-from passlib.context import CryptContext
-from database import Database
-from security import *
-from sendMail import Mail
+import uvicorn
+from fastapi import FastAPI
 
-db=Database()
-# mail=Mail()
+app = FastAPI()
 
-ACCESS_TOKEN_EXPIRE_MINUTES=40
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost:3000",
-    "http://localhost:3000/",
-    "http://192.168.0.128:3000/",
-    "https://miraparentpal.com",
-    "https://www.miraparentpal.com",
-    'https://miraparentpal.vercel.app'
-]
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI!"}
 
-app=FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
-# db={
-#     'vishnu@gmail.com':{
-#         'username':'vishnu',
-#         'full_name':'vishnu vardhan gowd',
-#         'email':'vishnu@gmail.com',
-#         'password':'$2b$12$1wWBGtrpd.I97eZYVyjG4ukcQTaszhV3Azmz4S5MGXo2ZJfqSxClm',
-#         'disabled':False,
-#         'invalid_entries':0
-#     }
-# }
-class FormData(BaseModel):
-    username:str = Form(...)
-    email:str = Form(...)
-    password:str = Form(...)
+# from asyncio import run
+# from fastapi import Depends, FastAPI, HTTPException, status,Form,Response,Request
+# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel
+# from datetime import datetime,timedelta
+# from jose import JWTError,jwt
+# from passlib.context import CryptContext
+# from database import Database
+# from security import *
+# from sendMail import Mail
 
-class Token(BaseModel):
-    access_token:str
-    email:str
-    username:str
-class TokenData(BaseModel):
-    email:str or None = None
+# db=Database()
+# # mail=Mail()
 
-class User(BaseModel):
-    username:str
-    email:str or None=None
-    full_name:str or None=None
-    disabled:bool or None=None
-    invalid_entries:int or None=None
-class UserInDB(User):
-    password:str
+# ACCESS_TOKEN_EXPIRE_MINUTES=40
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost:3000",
+#     "http://localhost:3000/",
+#     "http://192.168.0.128:3000/",
+#     "https://miraparentpal.com",
+#     "https://www.miraparentpal.com",
+#     'https://miraparentpal.vercel.app'
+# ]
+
+# app=FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# # db={
+# #     'vishnu@gmail.com':{
+# #         'username':'vishnu',
+# #         'full_name':'vishnu vardhan gowd',
+# #         'email':'vishnu@gmail.com',
+# #         'password':'$2b$12$1wWBGtrpd.I97eZYVyjG4ukcQTaszhV3Azmz4S5MGXo2ZJfqSxClm',
+# #         'disabled':False,
+# #         'invalid_entries':0
+# #     }
+# # }
+# class FormData(BaseModel):
+#     username:str = Form(...)
+#     email:str = Form(...)
+#     password:str = Form(...)
+
+# class Token(BaseModel):
+#     access_token:str
+#     email:str
+#     username:str
+# class TokenData(BaseModel):
+#     email:str or None = None
+
+# class User(BaseModel):
+#     username:str
+#     email:str or None=None
+#     full_name:str or None=None
+#     disabled:bool or None=None
+#     invalid_entries:int or None=None
+# class UserInDB(User):
+#     password:str
 
 
-@app.post('/auth/signup/')
-async def signup(request : Request):
+# @app.post('/auth/signup/')
+# async def signup(request : Request):
     
-    payload = await request.form()
-    payload=dict(payload)
-    payload['password']=get_password_hash(payload['password'])
-    user_details=payload
-    # user_details={
-    #     'username':username,
-    #     'email':email,
-    #     'password':get_password_hash(password)
-    # }
-    state=db.insert_user(user_details)
-    return state
-@app.post('/sendOTP/')
-async def getOTP(request:Request,username:str = Form(...),email:str = Form(...)):
-    user=db.get_user(email)
-    if user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='User already exists. ',
-            headers={'WWW-Authenticate':'Bearer'}
-        )
-    mail=Mail()
-    otp,msg=mail.sendOTP(username,email)
-    print(otp,msg)
-    if otp==None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f'{msg}',
-            headers={'WWW-Authenticate':'Bearer'}
-        )
-    return otp
+#     payload = await request.form()
+#     payload=dict(payload)
+#     payload['password']=get_password_hash(payload['password'])
+#     user_details=payload
+#     # user_details={
+#     #     'username':username,
+#     #     'email':email,
+#     #     'password':get_password_hash(password)
+#     # }
+#     state=db.insert_user(user_details)
+#     return state
+# @app.post('/sendOTP/')
+# async def getOTP(request:Request,username:str = Form(...),email:str = Form(...)):
+#     user=db.get_user(email)
+#     if user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail='User already exists. ',
+#             headers={'WWW-Authenticate':'Bearer'}
+#         )
+#     mail=Mail()
+#     otp,msg=mail.sendOTP(username,email)
+#     print(otp,msg)
+#     if otp==None:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail=f'{msg}',
+#             headers={'WWW-Authenticate':'Bearer'}
+#         )
+#     return otp
 
 
 
