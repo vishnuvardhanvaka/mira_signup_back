8007b79b1ef0ecdf76c9d8fc79cb031a2172b09d
Vishnu Vardhan Gowd Vaka <vishnuvardhanvaka1@gmail.com>
2024-01-10 10:37:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import uvicorn
 
 connection_url='mongodb+srv://vercel-admin-user:9fUSJGXbLKGD5jqZ@miraparentpal.ipmapar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 print(connection_url)
-# client=MongoClient(connection_url)
+client=MongoClient(connection_url)s
 # print('Client connection successful !')
 # database=client.babycare
 # login_collection=database.logins
